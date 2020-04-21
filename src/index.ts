e7985be659a5f1faf7abe993414e20ca9e82a494
ChenYong <chenyong@jimeng.io>
2020-04-21 15:23:22 +0800

    export dropdown props; alpha release

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as DropdownArea, useDropdownArea } from "./dropdown-area";
-export { default as DropdownMenu } from "./dropdown-menu";
+export { default as DropdownMenu, IDropdownMenuProps } from "./dropdown-menu";
 export { default as MenuList, IMenuListItem } from "./menu-list";
 export { default as ContentInput } from "./content-input";
 export { default as MenuTree } from "./menu-tree";
-export { default as DropdownTree } from "./dropdown-tree";
+export { default as DropdownTree, IDropdownTreeProps } from "./dropdown-tree";
